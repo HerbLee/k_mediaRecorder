66b54e413aaf46a43309c2f4a4af919391523ff7
herb2sy <herb2sy@gmail.com>
2019-09-04 08:40:29 +0800

    Update RecorderHelper.kt

--- lib/src/main/java/com/herb2sy/media/lib/recorder/RecorderHelper.kt
+++ lib/src/main/java/com/herb2sy/media/lib/recorder/RecorderHelper.kt
@@ -221,7 +221,6 @@ class RecorderHelper(val context:Context,val recorderView:RecorderView, val list
         mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
         mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
 
-        mediaRecorder?.setVideoFrameRate(25)
         mediaRecorder?.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
         mediaRecorder?.setVideoEncodingBitRate(videoEncodingBitRate* width*height)
 
@@ -352,8 +351,6 @@ class RecorderHelper(val context:Context,val recorderView:RecorderView, val list
                 parameters?.focusMode = "continuous-video"
             }
             parameters?.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO
-            parameters?.setPreviewFpsRange(25000,25000)
-            parameters?.previewFrameRate = 25
             mCamera?.parameters = parameters
             mCamera?.startPreview()
 
